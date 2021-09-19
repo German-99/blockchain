6821f0105f556f31d26d420dc8cb82fa278ace2f
Adrian <adrian2009@live.com.mx>
2021-09-19 01:58:50 -0700

    minado block

--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -1,3 +1,5 @@
+
+import { SHA256 } from 'crypto-js';
 class Block{
     constructor(timestamp, previousHash, hash, data){
         this.timestamp = timestamp;
@@ -8,7 +10,19 @@ class Block{
 
     static get genesis(){
         const timestamp =(new Date(2019,0,1)).getTime();
-        return new this(timestamp, undefined, 'g3n3sis-h4sh', 'g3n3sis-d44');
+        return new this(timestamp, undefined, 'g3n3sis-h4sh', 'transaccion0');
+    }
+
+    static mine(previousBlock, data){
+        const timestamp = Date.now();
+        const { hash: previousHash } = previousBlock;
+        const hash = Block.hash(timestamp, previousHash, data);
+
+        return new this(timestamp, previousHash, hash, data);
+    }
+
+    static hash(timestamp, previousHash, data){
+        return SHA256(`${timestamp}${previousHash}${data}`).toString();
     }
 
     toString(){
