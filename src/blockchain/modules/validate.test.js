9b13265a9aa82b3ac62d3ffbabd44367016195d2
Adrian <adrian2009@live.com.mx>
2022-03-14 22:40:00 -0700

    validar

--- src/blockchain/modules/validate.test.js
+++ src/blockchain/modules/validate.test.js
@@ -8,28 +8,10 @@ describe('validate()', () =>{
         blockchain = new Blockchain();
 });
 
-it('Validar cadena valida', () => {
-    blockchain.addBlock('bl0ck-1');
-    blockchain.addBlock('bl0ck-2');
+it ('Crear cadena valida' . () => {
 
-    expect(validate(blockchain.blocks)).toBe(true);
+    blockchain.addblock
+});
 
-    });
-
-    it('Invalidar la cadena con un genesis block corrupto'), () => {
-        blockchain.blocks[0].data = 'h4ck-data';
-
-        expect(() =>{
-            validate(blockchain.blocks);
-        }).toThrowError('Genesis block invalido');
-    });
-
-    it('Invalidar la cadena con un genesis block corrupto'), () => {
-        blockchain.blocks[0].data = 'h4ck-data';
-
-        expect(() =>{
-            validate(blockchain.blocks);
-        }).toThrowError('Genesis block invalido');
-    });
 });
 
